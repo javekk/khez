070a1b1c6106845e41d33b710a76436616262cbe
github-action-benchmark <github@users.noreply.github.com>
2026-04-26 15:17:00 +0000

    add Khez perft bench (customSmallerIsBetter) benchmark result for 832c3eb549efe269ba95c9c195c77ab367070f6a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1777214917040,
+  "lastUpdate": 1777216620984,
   "repoUrl": "https://github.com/javekk/khez",
   "entries": {
     "Khez perft bench": [
@@ -33,6 +33,45 @@ window.BENCHMARK_DATA = {
           {
             "name": "perft_nps",
             "value": 3418454,
+            "unit": "nodes/sec"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "periniraffaele@gmail.com",
+            "name": "javekk",
+            "username": "javekk"
+          },
+          "committer": {
+            "email": "periniraffaele@gmail.com",
+            "name": "javekk",
+            "username": "javekk"
+          },
+          "distinct": true,
+          "id": "832c3eb549efe269ba95c9c195c77ab367070f6a",
+          "message": "ci(bench): add benchmark with search engine",
+          "timestamp": "2026-04-26T17:15:48+02:00",
+          "tree_id": "8ba27feed3dae86b117ce0caaed9e0da5370512a",
+          "url": "https://github.com/javekk/khez/commit/832c3eb549efe269ba95c9c195c77ab367070f6a"
+        },
+        "date": 1777216620290,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "search_total_time_ms",
+            "value": 30679,
+            "unit": "ms"
+          },
+          {
+            "name": "search_total_nodes",
+            "value": 9949160,
+            "unit": "nodes"
+          },
+          {
+            "name": "search_nps",
+            "value": 324298,
             "unit": "nodes/sec"
           }
         ]
