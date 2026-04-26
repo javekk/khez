42b1428dbf08be86a635c4008ba7898c2226b0cc
javekk <periniraffaele@gmail.com>
2026-04-26 17:24:58 +0200

    prune perft-era bench entries

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -16,40 +16,6 @@ window.BENCHMARK_DATA = {
             "username": "javekk"
           },
           "distinct": true,
-          "id": "e3fef2d77fd86e5eb8c4d67c307aee00db6beaa2",
-          "message": "init gh-pages",
-          "timestamp": "2026-04-26T16:46:06+02:00",
-          "tree_id": "5844a46b4290df8543d51e6dcb7a928c61bc18d5",
-          "url": "https://github.com/javekk/khez/commit/e3fef2d77fd86e5eb8c4d67c307aee00db6beaa2"
-        },
-        "date": 1777214916237,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "perft_total_time_ms",
-            "value": 106278,
-            "unit": "ms"
-          },
-          {
-            "name": "perft_nps",
-            "value": 3418454,
-            "unit": "nodes/sec"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "periniraffaele@gmail.com",
-            "name": "javekk",
-            "username": "javekk"
-          },
-          "committer": {
-            "email": "periniraffaele@gmail.com",
-            "name": "javekk",
-            "username": "javekk"
-          },
-          "distinct": true,
           "id": "832c3eb549efe269ba95c9c195c77ab367070f6a",
           "message": "ci(bench): add benchmark with search engine",
           "timestamp": "2026-04-26T17:15:48+02:00",
